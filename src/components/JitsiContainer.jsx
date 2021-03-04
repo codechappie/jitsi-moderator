78afe377aa1d6964cc6106b76e7b5359d121b90a
codechappie <davidproyecto01@gmail.com>
2021-03-04 16:55:42 -0500

    test

--- src/components/JitsiContainer.jsx
+++ src/components/JitsiContainer.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const JitsiContainer = ({classroom}) => {
-
+const JitsiContainer = ({ classroom }) => {
   return (
     <div className="jitsi-container">
-      <iframe
-        id="myFrame"
-        src={`https://moderated.jitsi.net/${classroom}`}
-        title="transmision"
-        frameBorder="0"
-      ></iframe>
+      <a
+        href={`https://moderated.jitsi.net/${classroom}`}
+        target="_blank"
+        rel="noreferrer"
+      >Ingresar al salon como profesor</a>
     </div>
   );
 };
